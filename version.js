b4913e75eb16d3ccef65a28e4a7e2fdbb01eede7
IttayEyal <ittay.eyal@gmail.com>
2026-04-02 12:46:32 +0300

    Update app version to v1.0.1 in version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "v1.0";
+const APP_VERSION = "v1.0.1";
 document.addEventListener("DOMContentLoaded", function () {
     const el = document.getElementById("app-version");
     if (el) el.textContent = APP_VERSION;
